ec63b6250a6c9ab5fd5b6b368cc2b7028bcca0ae
Guilherme Moretti <68204019+Guirraa@users.noreply.github.com>
2021-05-31 10:15:05 -0300

    check files and add functions
    check for directories and add option in the menu

--- script_backup.sh
+++ script_backup.sh
@@ -2,37 +2,35 @@
 
 #? Definindo as variaveis para utilizar no script
 
-DATE=$(date +%d-%m-%Y-%Hh-%Mm)          #? Informa a data no instante que o script será rodado 
-PASTA_BACKUP_INPUT="$HOME/.pastasparabackup.txt"    #? Informa as pastas que serão "backupiadas" ou seja, as pastas que serão salvas
+DATE=$(date +%d-%m-%Y-%Hh-%Mm)          #? Informa a data no instante que o script foi rodado
+PASTA_BACKUP_INPUT="$HOME/.pastasparabackup.txt"    #? Informa as pastas que serão "backupeadas" ou seja, as pastas que serão salvas
 LOCAL_FILE_SAVE="$HOME/Backup/snapshots/backup-${DATE}.tar.gz" #? Informa o local em que será salvo o arquivo de backup + o nome do arquivo
 LOCAL_DIRETORIO="$HOME/Backup/snapshots" #? Pasta onde serão salvo os backups
 NUMBER_OF_FILES_BACKUP=$(ls $LOCAL_DIRETORIO | wc -l) #? Informa a quantidade de backups que estão salvos na pasta
-DIFERENCA=$(expr 6 - $NUMBER_OF_FILES_BACKUP) #? Informa a diferença entre a quantidade de backups (menos) - 5 (Valor que defini como excesso de backup) se
-
+DIFERENCA=$(expr 5 - $NUMBER_OF_FILES_BACKUP) #? Informa a diferença entre a quantidade de backups (menos) - 5 (Valor que defini como excesso de backup) se
 limpezabackups()        #? Bloco para limpar o excesso de backups
 {
     if [ $NUMBER_OF_FILES_BACKUP -gt 5 ]        #? Se o numero de backups salvos no diretorio foi mais que 5...
     then
-        cd $LOCAL_DIRETORIO && ls -1 -t | tail $DIFERENCA | xargs rm #? Entre no local do diretorio e apague o excesso de backup.
+        cd $LOCAL_DIRETORIO && ls -1 -t | tail $DIFERENCA | xargs rm -f #? Entre no local do diretorio e apague o excesso de backup.
     fi
 }
-
 compress()      #? Bloco para compactar os backups
 {
-    if [ ! -d $LOCAL_DIRETORIO ]    #? Verifica se o diretorio de backups existe. se não...
-    then
-        mkdir $LOCAL_DIRETORIO #? Faça o diretorio
+    if [[ ! -e $PASTA_BACKUP_INPUT && ! -d $LOCAL_DIRETORIO ]]
+        then
+        clear
+        echo -e "Pasta de backup e/ou arquivo de configuração de pasta não existem."
+        printf "Deseja recriar os arquivos? (s/n) "
+        read -r opcao1
+        case $opcao1 in
+        s) criation && addfolders;;
+        n) echo -e "Erro: Pasta para backup não encontrada" && return 1;;
+        esac
     fi
-    
-    if [ ! -f $PASTA_BACKUP_INPUT]      #? Verifica se o txt existe
-    then
-        touch $PASTA_BACKUP_INPUT
-    fi
-
-    cd $HOME
+    addfolders
     clear
-    echo -e "\t\t\t\tItens salvos"
-    if tar -czvf $LOCAL_FILE_SAVE -T $PASTA_BACKUP_INPUT        #? Compacta as pastas que foram escolhidas para serem salvas num unico arquivo tar.gz.
+    if tar -czvf "$LOCAL_FILE_SAVE" -T <(sed 1,2d "$PASTA_BACKUP_INPUT")   #? Compacta as pastas que foram escolhidas para serem salvas num unico arquivo tar.gz.
     then
         echo -e "\n\t\t\t\tBackup feito"
     else
@@ -40,13 +38,41 @@ compress()      #? Bloco para compactar os backups
         exit 1
     fi
 }
-
+addfolders()
+{
+    while ! tail -n +3 $PASTA_BACKUP_INPUT| grep -c -m1 -o /
+    do
+        clear
+        echo -e "Insira o diretorio das pastas que deseja fazer backup."
+        printf "Deseja abrir o editor de texto? (s/n)"
+        read -r opcao2
+            case $opcao2 in
+                s) nano $PASTA_BACKUP_INPUT;;
+                n) exit 1;;
+            esac
+    done
+}
+criation()
+{
+    if [[ ! -e $PASTA_BACKUP_INPUT && ! -d $LOCAL_DIRETORIO ]]
+        then
+            touch $PASTA_BACKUP_INPUT && mkdir $LOCAL_DIRETORIO
+            echo -e "#Coloque o diretorio da pasta que será salva com o caminho COMPLETO.\n#Insira no seguinte modelo:/home/user/pasta" > $PASTA_BACKUP_INPUT
+            echo -e "Arquivos criados com sucesso"
+        else
+            echo -e "Arquivos ja existem"
+    fi
+}   
+menu()
+{
+clear
 echo -e "-------------------------------------------------------------------------------"
 echo -e "                            Digite a opção desejada                            "
 echo -e "1. Fazer backup"
 echo -e "2. Apagar excesso de backup"
 echo -e "3. Completo (Limpeza + Backup)"
-echo -e "9. Criar txt para adicionar as pastas desejadas para backup"
+echo -e "4. Resetar configurações das pastas desejadas para backup"
+echo -e "5. Editar configurações das pastas desejadas para backup"
 echo -e "-------------------------------------------------------------------------------"
 printf "Digite a opção desejada: " 
 read -r opcao       #? Receba a opção desejada
@@ -55,9 +81,8 @@ case $opcao in      #? Execute x de acordo com a opção desejada
 1) compress ;;
 2) limpezabackups ;;
 3) limpezabackups && compress ;;
-9) if [ ! -f $PASTA_BACKUP_INPUT ]    
-    then
-        touch $PASTA_BACKUP_INPUT
-    fi;;
+4) rm -f $PASTA_BACKUP_INPUT && rm -rf $LOCAL_DIRETORIO && criation;;
+5) nano $PASTA_BACKUP_INPUT;;
 esac
-
+}
+menu
